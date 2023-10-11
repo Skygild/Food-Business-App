6733864944b489f0344b2c858b9ee87bce1f1e44
Lev Skyler Roan <skylevroan@gmail.com>
2023-10-11 19:45:54 +0800

    v1.01 frontend - Modified for Login and Sign up page

--- client/frontend/src/header.jsx
+++ client/frontend/src/header.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    navigate("/login");
+  };
+
+  const handleSignup = () => {
+    navigate("/signup");
+  };
+
   return (
     <header>
       <div className="buttons">
-        <button>Sign up</button>
-        <button>Log in</button>
+        <button onClick={handleSignup}>Sign up</button>
+        <button onClick={handleLogin}>Log in</button>
       </div>
     </header>
   );
--- client/frontend/src/index.js
+++ client/frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import SignupPage from "./SignupPage";
+import LoginPage from "./LoginPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
